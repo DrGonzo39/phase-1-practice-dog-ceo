455c64aa5cecb6eeded1132fcada8c9629e82220
Arthur Benware <artpolitic91@gmail.com>
2022-10-24 10:36:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(response => response.json())
+        .then(data => {
+            data.message.forEach((element) => {
+                let dogImage = document.createElement('img')
+                dogImage.src = element
+                let div = document.querySelector('div')
+                div.appendChild(dogImage)
+            })
+        })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(response => response.json())
+        .then(data => {
+            for (const key in data.message) {
+                let li = document.createElement('li')
+                li.textContent = key
+                let ul = document.getElementById('dog-breeds')
+                ul.appendChild(li)
+            }
+        })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const li = document.querySelectorAll('li');
+    li.addEventListener('click', (event) => {
+        event.target.style.color = 'green';
+    })
+})
